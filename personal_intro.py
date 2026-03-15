668bbeea99baaf5bbd458d83abe413966e37bbcf
rutuja0327 <thalkarirutuja@gmail.com>
2026-03-16 00:16:00 +0530

    Create personal_intro.py

---
+++ personal_intro.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+hobby = input("What is your favorite hobby? ")
+
+print("\n🎉 Welcome", name, "! 🎉")
+print(f"You are {age} years old and love {hobby}.")
